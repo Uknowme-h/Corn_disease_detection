a3d981d12d2656addbd9c7710e9954d4380f34c9
Nishant-jswl <152166858+Uknowme-h@users.noreply.github.com>
2024-05-02 23:02:26 +0545

    finished setting up docker

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from model.model import predict_image
-
+from app.model.model import predict_image
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Hello, World"}
+
+@app.get("/predict/")
+def predict():
+    # if not image_url:
+    #     return {"error": "image_url is required"}
+    
+    try:
+        prediction = predict_image('./app/model/example2.jpg')
+        return {"prediction": prediction}
+    except Exception as e:
+        return {"error": str(e)}
--- app/model/model.py
+++ app/model/model.py
@@ -1,17 +1,23 @@
-from fastai import *
+# from io import BytesIO
 from fastai.vision.all import *
+# import requests
+# from PIL import Image
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
-
-def predict_image(image_path):
+def predict_image(image_url):
+    # Download the image
+    # response = requests.get(image_url)
+    # with Image.open(BytesIO(response.content)) as img:
+        
+    img = PILImage.create(image_url)
+    
     # Load the Fastai model
-    learn = load_learner('./model/corn_model.pkl')
-
-    img = PILImage.create(image_path)
+    learn = load_learner('./app/model/corn_model.pkl')
 
     # Make the prediction
     pred_class, pred_idx, outputs = learn.predict(img)
 
     # Return the prediction
     return pred_class
-
-#  Set-ExecutionPolicy Unrestricted -Scope Process
